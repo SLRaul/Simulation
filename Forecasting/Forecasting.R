eb6088dfe25a5b0123e990fb54811bc62ff1bbb8
Rls <slraul@DESKTOP-P0JBGN9.localdomain>
2021-10-12 18:02:14 -0300

    add sub caps 3.3, 3.2 e 3.1

--- Forecasting/Forecasting.R
+++ Forecasting/Forecasting.R
@@ -279,4 +279,85 @@ gridExtra::grid.arrange(
 ############ Cap 3 ferramentas de forecast ##############
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+## os métodos mais simple e q são tomados como referência são
+
+## médias móveis
+# meanf(time series, periododa ts)
+
+## método naive
+# naive(time series, periododa ts)
+# rwf(y, h) # Equivalent alternative
+
+## método naive sasonal
+# snaive(time series, periododa ts)
+
+# # método drift
+# rwf(time series, periododa ts, drift=T)
+
+beer_exemple <- window(ausbeer, start =1994, end = c(2004, 6))
+
+#contruindo o gráfico
+autoplot(beer_exemple) +
+  autolayer(meanf(beer_exemple, h = 7), series = "Média", PI = F) +
+  autolayer(naive(beer_exemple, h = 7), series = "Naive", PI = F) +
+  autolayer(snaive(beer_exemple, h = 7), series = "Sasonal Naive", PI = F) +
+  ggtitle("Produção quadrimestral de cerveja") +
+  xlab('Tempo') + ylab("Produção em megalitros")
+
+autoplot(goog200) +
+  autolayer(meanf(goog200, h = 20), series = "Média", PI = F) +
+  autolayer(naive(goog200, h = 20), series = "Naive", PI = F) +
+  autolayer(snaive(goog200, h = 20), series = "Sasonal Naive", PI = F) +
+  ggtitle("Google stock (até 06/2013)") +
+  xlab('Tempo') + ylab("Preço")
+
+# # tranformações e ajuste para um ST
+# mes para dia (dividindo a produção do mes pela quantidade  de dias)
+# exemplo
+exemplo <- cbind(mensal = milk, media_diaria = milk/monthdays(milk))
+
+autoplot(exemplo, facets = T) +
+  ggtitle("Prod leite por vaca") +
+  xlab('mês/ano') + ylab('prod')
+
+# # ajuste de populaçãp
+# dividir pelo tamanho da pop 
+
+# # Ajuste de inflação
+# fazer a correção da inflação com dados de preço
+
+# # transformações matematicas
+# log, ..
+# box cox
+# a escolha do lamb pode ter como base o resultado da função
+BoxCox.lambda(elec)
+
+autoplot(BoxCox(elec, BoxCox.lambda(elec)))
+
+# fazendo o ajuste de viez com o boxcox
+# com o inverso
+autoplot(InvBoxCox(elec, BoxCox.lambda(elec), biasadj = T, fvar = c(-1,1)))
+# com box cox (ts, metodo drift, labda do boxcox (tranformação), periodo estimado, nivel de conf )
+fc <- rwf(eggs, drift=TRUE, lambda =0, h = 50, level =80)
+fc2 <- rwf(eggs, drift=TRUE, lambda=0, h=50, level=80,
+           biasadj=TRUE)
+autoplot(eggs) +
+  autolayer(fc, series="Simple back transformation") +
+  autolayer(fc2, series="Bias adjusted", PI=FALSE) +
+  guides(colour=guide_legend(title="Forecast"))
+
+# # # fazendo o ajuste
+
+autoplot(goog200)
+
+residuo <- residuals(naive(goog200))
+autoplot(residuo)
+
+gghistogram(residuo)
+
+ggAcf(residuo)
+
+# checando os resíduos
+checkresiduals(naive(goog200))
+
 # # # https://otexts.com/fpp2/graphics-exercises.html
